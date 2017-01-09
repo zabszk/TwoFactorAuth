700041bfe53057fd35a9014e227e462c57a8c879
zabszk <zabszk@protonmail.ch>
2017-01-09 09:38:55 +0100

    Version 1.1, fixed lang issue

--- TwoFactorAuth/src/net/zabszk/TFA/GenerateLang.java
+++ TwoFactorAuth/src/net/zabszk/TFA/GenerateLang.java
@@ -2,6 +2,7 @@ package net.zabszk.TFA;
 
 import java.io.File;
 import java.io.InputStream;
+import java.net.URLDecoder;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.jar.JarFile;
@@ -15,22 +16,11 @@ public class GenerateLang {
 		{
 			try 
 			{
-				//File file = new File(Main.class.getResource("/resources/" + name + ".yml").getFile());
-				JarFile file = new JarFile(Main.class.getProtectionDomain().getCodeSource().getLocation().getPath());
+				JarFile file = new JarFile(URLDecoder.decode(Main.class.getProtectionDomain().getCodeSource().getLocation().getPath()));
 				ZipEntry entry = file.getEntry("resources/" + name + ".yml");
 				InputStream inputStream = file.getInputStream(entry);
 				
-				
 				Files.copy(inputStream, Paths.get("plugins/TwoFactorAuth/lang/" + name + ".yml"));
-			    /*BufferedReader br = new BufferedReader(new FileReader(file));
-			    BufferedWriter bw = new BufferedWriter(new FileWriter(langfile));
-			    String l;
-			    while((l=br.readLine())!=null){
-			        bw.write(l);
-			        bw.newLine();
-			    }
-			    
-			    bw.close();*/
 			}
 			catch (Exception e)
 			{
